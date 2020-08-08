e2d82fb8a2ebb8c630e02a3f992865335f46e518
nightwishh <65540986+nightwishh@users.noreply.github.com>
2020-08-09 00:02:04 +0400

    antsge - with ionic. latest (Grid Component added)

--- platforms/android/app/src/main/assets/www/polyfills-es5.js
+++ platforms/android/app/src/main/assets/www/polyfills-es5.js
@@ -18899,7 +18899,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! /Users/gigi/Documents/GitHub/ants copy/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
+    /*! /Users/gigi/Documents/GitHub/ants---ionic/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -18907,7 +18907,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     module.exports = __webpack_require__(
-    /*! /Users/gigi/Documents/GitHub/ants copy/src/polyfills.ts */
+    /*! /Users/gigi/Documents/GitHub/ants---ionic/src/polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
